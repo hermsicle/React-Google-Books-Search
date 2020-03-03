b4a8ded343a2ee0741a440f8b38c23081a8684dc
Herman Liu <hermanliu95@gmail.com>
2020-03-03 15:50:54 -0800

    added books schema

---
+++ models/GoogleBooks.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {
+        type: String
+    },
+    authors: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: Objectid
+    },
+    link: {
+        type: String
+    }
+})
+
+const GoogleBooks = mongoose.model("GoogleBooks", BookSchema);
+
+module.exports = GoogleBooks;

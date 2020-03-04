704a3667e89e9f873045852acdbb0638a19a3d29
Herman Liu <hermanliu95@gmail.com>
2020-03-03 16:14:04 -0800

    installed react router dom npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route>
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

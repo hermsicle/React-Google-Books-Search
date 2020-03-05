d6d3db3cadbfb80e88230eeb8b02181e0d7e8936
Herman Liu <hermanliu95@gmail.com>
2020-03-04 16:04:05 -0800

    changed to a rcc in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Search from './components/Search/Search';
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super();
+    this.setState = {};
+  }
 
 
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Nav />
+          <Switch>
+            <Route>
+              <Search></Search>
+            </Route>
+            <Route>
 
-
-  return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Switch>
-          <Route>
-            <Search></Search>
-          </Route>
-          <Route>
-
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;

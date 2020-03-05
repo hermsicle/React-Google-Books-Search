8207be62a36bf09fb8fb75cdc9203567b16cda1b
Herman Liu <hermanliu95@gmail.com>
2020-03-04 16:38:34 -0800

    imported Link from react-router-dom and linked the home and saved page

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Search from './components/Search/Search';
+import axios from 'axios'
+import Results from './components/Results/Results';
+import Saved from './components/Saved/Saved';
 
 class App extends Component {
 
   constructor() {
     super();
-    this.setState = {};
+    this.setState =
+    {
+      books: []
+    };
   }
 
+
+  getBooks = () => {
+    axios.get('/')
+  }
 
   render() {
     return (
       <div className="App">
         <Router>
           <Nav />
+          <Link to="/" className="link">Home</Link>
+          <br></br>
+          <Link to="/saved" className="link">Saved Books</Link>
           <Switch>
-            <Route>
-              <Search></Search>
-            </Route>
-            <Route>
 
+            <Route exact path="/">
+              <Search></Search>
+              <Results></Results>
             </Route>
+
+            <Route exact path="/saved">
+              <Saved></Saved>
+            </Route>
+
           </Switch>
         </Router>
       </div>
--- client/src/components/Saved/Saved.js
+++ client/src/components/Saved/Saved.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Saved() {
     return (
         <div>
-
+            <h1>Hello from the saved page</h1>
         </div>
     )
 }
